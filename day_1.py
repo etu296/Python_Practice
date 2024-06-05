152ffcd4031edfd35f844ed4f6f66b47372e7803
D:/Cypress/Orange HRM <etumahmuda@gmail.com>
2024-06-05 16:29:12 +0600

    day 1 practice

---
+++ day_1.py
@@ -0,0 +1,37 @@
+#print("hello, etu")
+x = 4
+X = "etu mahmuda"
+#print(x)
+#print(X) # case sensetive captital letter & small letter define different value
+#puthon comment starts with #
+
+z = float(3)
+#print(z)
+#print(type(z))
+#variable names are case-sensitive
+#camel case
+myName = X
+#print(X)
+#camel case
+MyNameEtu = X
+#print(X)
+#snake case
+my_name_etu = X
+#print(X)
+#Many Values to Multiple Variables
+a , b , c = "Khadiza" , "Dulon" , "Sorna"
+print(a , b , c)
+print(b)
+print(c)
+#One Value to Multiple Variables
+A = B = C = "Lily "
+print(A + B + c)
+print(B)
+print(C)
+#Unpack a Collection
+Car = ["BMW","Honda","Tesla","Yamaha"]
+d , e , f , g = Car
+print(d)
+print(e)
+print(f)
+print(g)
